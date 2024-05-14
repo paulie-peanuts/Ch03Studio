fb5b1a301d2162396ad153a36d6857b61f29d7e9
PauliePnuts <91554001+PauliePnuts@users.noreply.github.com>
2024-05-14 15:21:10 -0500

    studio

---
+++ Ch03BB/Program.cs
@@ -0,0 +1,25 @@
+﻿string letters = "abcdefghijklmnopqrstuvwxyz";
+// Console.WriteLine("What word would you like to use?");
+string text = File.ReadAllText("./words.txt");
+
+// string text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan sem ut ligula scelerisque sollicitudin. Ut at sagittis augue. Praesent quis rhoncus justo. Aliquam erat volutpat. Donec sit amet suscipit metus, non lobortis massa. Vestibulum augue ex, dapibus ac suscipit vel, volutpat eget massa. Donec nec velit non ligula efficitur luctus.";
+
+Dictionary<char, int> charCounter = [];
+foreach (char character in text.ToCharArray())
+{
+    if (letters.Contains(character))
+    {
+        if (!charCounter.ContainsKey(character))
+        {
+            charCounter.Add(character, 1);
+        }
+        else
+        {
+            charCounter[character]++;
+        }
+    }
+}
+foreach (KeyValuePair<char, int> kvp in charCounter)
+{
+    Console.WriteLine(kvp.Key + ": " + kvp.Value);
+}
---
+++ Ch3Studio/Program.cs
@@ -0,0 +1,17 @@
+﻿string text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan sem ut ligula scelerisque sollicitudin. Ut at sagittis augue. Praesent quis rhoncus justo. Aliquam erat volutpat. Donec sit amet suscipit metus, non lobortis massa. Vestibulum augue ex, dapibus ac suscipit vel, volutpat eget massa. Donec nec velit non ligula efficitur luctus.";
+
+Dictionary<char, int> charCounter= [];
+foreach (char character in text.ToCharArray())
+{
+    if (!charCounter.ContainsKey(character))
+    {
+        charCounter.Add(character, 1);
+    } else 
+    {
+        charCounter[character] ++;
+    }
+}
+foreach (KeyValuePair<char, int> kvp in charCounter)
+{
+    Console.WriteLine(kvp.Key + ": " + kvp.Value);
+}
